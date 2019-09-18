6164979b26147f76278fad225f6359228b033520
Sam Hiatt <samhiatt@gmail.com>
2019-09-17 17:42:16 -0700

    Adding licenses

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 /*
  Header tag link extraction script copied from:
  https://github.com/pages-themes/leap-day/blob/2378f92b7cbede0703231d2e63a85b536585f260/assets/js/main.js
-
+ CC0 license: https://github.com/pages-themes/leap-day/blob/2378f92b7cbede0703231d2e63a85b536585f260/LICENSE
 */
 $(".post-content h1, .post-content h2, .post-content h3").each(function(){
   $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
   $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
   $("nav ul li:first-child a").parent().addClass("active");
 });
-console.log($(".post-content h1, .post-content h2, .post-content h3"));
--- credits.md
+++ credits.md
@@ -4,4 +4,4 @@ title: Credits
 permalink: /credits/
 ---
 
-This page was built using [Jekyll](https://jekyllrb.com) and derived from the [minima theme](https://github.com/jekyll/minima).
+This page was built using [Jekyll](https://jekyllrb.com) and derived from the [minima theme](https://github.com/jekyll/minima/tree/v2.5.0).
